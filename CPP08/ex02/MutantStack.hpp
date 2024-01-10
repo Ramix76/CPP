86690c2f539546134b56f5b611b1893f15ecd4b5
Ramix76 <framos-p@car8s4.42barcelona.com>
2024-01-10 12:04:40 +0100

    starting CPP08/ex02

---
+++ CPP08/ex02/MutantStack.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: framos-p <framos-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 11:57:50 by framos-p          #+#    #+#             */
+/*   Updated: 2024/01/10 12:01:49 by framos-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+# include <iostream>
+# include <algorithm>
+# include <string>
+
+
+
+#endif
